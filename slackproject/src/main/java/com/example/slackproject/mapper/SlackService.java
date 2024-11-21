05d37c8cc48e5c4910cf5641ea992c865175f91b
heesunkimm <khsun4624@naver.com>
2024-11-21 17:01:14 +0900

    feat: slack 연동 및 테스트 메세지 전송

---
+++ slackproject/src/main/java/com/example/slackproject/mapper/SlackService.java
@@ -0,0 +1,36 @@
+package com.example.slackproject.mapper;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.slack.api.Slack;
+import com.slack.api.methods.SlackApiException;
+import com.slack.api.methods.request.chat.ChatPostMessageRequest;
+import com.slack.api.methods.response.chat.ChatPostMessageResponse;
+
+@Service
+public class SlackService {
+	
+	@Value("${slack.token}")
+	private String slackToken;
+	
+	@Value("${slack.channel}")
+    private String slackChannel;
+	
+	
+	public void sendMessage(String message) throws IOException, SlackApiException {
+		Slack slack = Slack.getInstance();
+		ChatPostMessageRequest request = ChatPostMessageRequest.builder()
+				.channel(slackChannel)
+				.text(message)
+				.build();
+		
+		ChatPostMessageResponse response = slack.methods(slackToken).chatPostMessage(request);
+		
+		if(!response.isOk()) {
+			throw new RuntimeException("Error Message: " + response.getError());
+		}
+	}
+}
